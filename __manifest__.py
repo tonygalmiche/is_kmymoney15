db885b03b9fb3eefb03a97f36a25ca1b97ca0043
Tony Galmiche <tony.galmiche@infosaone.com>
2021-11-21 11:19:39 +0100

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name'     : 'InfoSaône - Module Odoo 15 kMyMoney',
+    'version'  : '0.1',
+    'author'   : 'InfoSaône',
+    'category' : 'InfoSaône',
+    'description': """
+InfoSaône - Module Odoo 15 Module Odoo kMyMoney
+===================================================
+""",
+    'maintainer' : 'InfoSaône',
+    'website'    : 'http://www.infosaone.com',
+    'depends'    : [
+        'base',
+    ],
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/is_kmymoney_view.xml',
+        'views/is_kmymoney_report_view.xml',
+        'report/solde_par_mois_report.xml',
+        'report/solde_par_an_report.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ report/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import solde_par_mois_report
+from . import solde_par_an_report
+
+
+
+
